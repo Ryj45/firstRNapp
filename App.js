211e482e3d6664a18b2cfcc7e15961d5e12aa9e6
18039253 <18039253@myrp.edu.sg>
2019-11-11 15:54:18 +0800

    18039253 second

--- App.js
+++ App.js
@@ -46,6 +46,24 @@ const App: () => React$Node = () => {
               </Text>
               <Text style={styles.highlight}>RP Furniture Shop</Text>
             </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
             <LearnMoreLinks />
           </View>
         </ScrollView>
